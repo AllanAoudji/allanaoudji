bd8cd6001e4710fa0d91b5407a14e9b78e553ba2
AllanAoudji <allan.jouannet@protonmail.com>
2025-07-06 17:58:56 +0200

    style Title component

--- components/title.tsx
+++ components/title.tsx
@@ -3,5 +3,9 @@ type props = {
 };
 
 export default function Title({ children }: props) {
-	return <h1 className="text-3xl uppercase">{children}</h1>;
+	return (
+		<h1 className="border-b-4 bg-amber-200 px-8 py-5 text-4xl font-bold text-green-600 uppercase">
+			{children}
+		</h1>
+	);
 }
