3e2b5b2ff63708c5306018e7b00311b9871c84e2
AllanAoudji <allan.jouannet@protonmail.com>
2025-07-10 09:50:14 +0200

    import and use Adobe Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en">
-			<body className="flex h-screen flex-col justify-between antialiased">
+			<body className="font-gopher flex h-screen flex-col justify-between antialiased">
 				<header>
 					<Menu />
 				</header>
