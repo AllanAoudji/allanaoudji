fbfb6f5e1d5e4e399dcdc1057934c7bc47e67227
AllanAoudji <allan.jouannet@protonmail.com>
2025-07-12 13:39:47 +0200

    setup sanity

---
+++ app/(back)/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+
+export const metadata: Metadata = {
+	title: "Allan Aoudji",
+	description: "Allan Aoudji | graphiste & illustrateur",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html lang="en">
+			<body className="antialiased">
+				<main>{children}</main>
+			</body>
+		</html>
+	);
+}
