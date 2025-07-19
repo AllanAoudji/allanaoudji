ad1193f6ad8c7f6085901813a09facb3d188672e
AllanAoudji <allan.jouannet@protonmail.com>
2025-07-19 15:16:21 +0200

    throw error if fetch failed on ContactSection and WorksHomeSection

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,13 +1,23 @@
 import ContactSectionContainer from "./ContactSectionContainer";
 import ContactSectionItem from "./ContactSectionItem";
 import { getContacts } from "@/sanity/lib/queries";
+import { CONTACTS_QUERYResult } from "@/sanity/types";
 
 export default async function ContactSection() {
-	const query = await getContacts();
+	let query: CONTACTS_QUERYResult;
+	try {
+		query = await getContacts();
+	} catch (error) {
+		if (error instanceof Error) {
+			throw error;
+		}
+		throw new Error("fetch failed");
+	}
 
 	if (!query || !query.contacts || query.contacts.length === 0) {
 		return null;
 	}
+
 	return (
 		<ContactSectionContainer>
 			<ul className="items-gap grid-default">
--- components/WorksHomeSection.tsx
+++ components/WorksHomeSection.tsx
@@ -2,9 +2,18 @@ import Link from "next/link";
 import ImageContainer from "./ImageContainer";
 import WorksHomeSectionContainer from "./WorksHomeSectionContainer";
 import { getWorks } from "@/sanity/lib/queries";
+import { WORKS_QUERYResult } from "@/sanity/types";
 
 export default async function WorksSection() {
-	const query = await getWorks("gallery");
+	let query: WORKS_QUERYResult;
+	try {
+		query = await getWorks("gallery");
+	} catch (error) {
+		if (error instanceof Error) {
+			throw error;
+		}
+		throw new Error("fetch failed");
+	}
 
 	if (!query || !query.works || query.works.length == 0) {
 		return null;
