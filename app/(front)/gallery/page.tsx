754491ca6705926622824b147121edbcb0af47ac
AllanAoudji <allan.jouannet@protonmail.com>
2025-11-20 19:07:47 +0100

    clean Gallery page file

--- app/(front)/gallery/page.tsx
+++ app/(front)/gallery/page.tsx
@@ -2,34 +2,45 @@ import { Suspense } from "react";
 import Title from "@/components/Title";
 import WorksGallerySection from "@/components/WorksGallerySection";
 import { getWorks } from "@/sanity/lib/queries";
+import { WORKS_QUERYResult } from "@/sanity/types";
 
 const GalleryLoader = async () => {
-	const query = await getWorks("gallery");
-	return (
-		// TODO: create fallback
-		<>
-			<Title>galerie</Title>
-			{!!query && query.works && !!query.works.length ? (
-				query.works.map((work, i) => (
-					<WorksGallerySection
-						key={work._id}
-						separator={query.works ? query.works.length - 1 !== i : false}
-						work={work}
-					/>
-				))
-			) : (
-				<div>
-					<p>Empty</p>
-				</div>
-			)}
-		</>
-	);
+	let query: WORKS_QUERYResult;
+
+	try {
+		query = await getWorks("gallery");
+	} catch (error) {
+		if (error instanceof Error) {
+			throw error;
+		}
+		throw new Error("fetch failed");
+	}
+
+	if (!query || !query.works || !query.works.length) {
+		return (
+			<div>
+				<p>Empty</p>
+			</div>
+		);
+	}
+
+	return query.works.map((work, i) => (
+		<WorksGallerySection
+			key={work._id}
+			separator={query.works ? query.works.length - 1 !== i : false}
+			work={work}
+		/>
+	));
 };
 
+// TODO: create fallback
 export default function Gallery() {
 	return (
-		<Suspense fallback={<p>Loading...</p>}>
-			<GalleryLoader />
-		</Suspense>
+		<>
+			<Title>galerie</Title>
+			<Suspense fallback={<p>Loading...</p>}>
+				<GalleryLoader />
+			</Suspense>
+		</>
 	);
 }
