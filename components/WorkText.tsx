13488a80b6ed7ff1a0e1888a0ad21387c2596329
AllanAoudji <allan.jouannet@protonmail.com>
2026-03-20 16:02:08 +0100

    add className props to workText component

--- components/WorkText.tsx
+++ components/WorkText.tsx
@@ -9,7 +9,7 @@ export default function WorkText({ className, text }: Readonly<Props>) {
 	if (!text) return null;
 	const paragraphs = text.split("\n").filter(Boolean) ?? [];
 	return (
-		<div className="pb-4 text-sm leading-4.5 tracking-[0.01em]">
+		<div className={cn("pb-4 text-sm leading-4.5 tracking-[0.01em]", className)}>
 			{paragraphs.map((paragraph, index) => (
 				<p key={index} className={index === 0 ? "" : "indent-8"}>
 					{paragraph}
