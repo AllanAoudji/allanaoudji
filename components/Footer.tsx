71728227ad19868bdacdc905998d94752ba7a50f
AllanAoudji <allan.jouannet@protonmail.com>
2026-03-23 22:17:29 +0100

    finish about/CGV/legal notices/privacy policy error/suspense logic

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import { Suspense } from "react";
+import FooterCopyright from "./FooterCopyright";
+import FooterMenu from "./FooterMenu";
+
+export default function Footer() {
+	return (
+		<footer className="padding-container pt-20 pb-8 text-sm">
+			<FooterMenu />
+			<Suspense>
+				<FooterCopyright />
+			</Suspense>
+		</footer>
+	);
+}
